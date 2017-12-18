516638a7397e0785bfca6920da4c3bef5f23a356
Justin Brazeau <justinb@live.ca>
2017-12-18 10:13:41 +0700

    fix meta viewport

--- app/js/components/HeadInfo.jsx
+++ app/js/components/HeadInfo.jsx
@@ -7,6 +7,7 @@ class HeadInfo extends React.Component {
     return (
       <Helmet>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
 
         <meta name="google-site-verification" content="wVHCqj5uzKUKHdqVdAumb88mi6tWeMv8vOLD5ri5ysk" />
